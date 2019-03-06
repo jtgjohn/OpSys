fe02467637616879dbf2001b553787960768c7bc
jtgjohn <gayj@rpi.edu>
2019-03-06 18:08:12 -0500

    added random

--- project1/project1.py
+++ project1/project1.py
@@ -1,7 +1,31 @@
 import sys
 
 
+############################################################################
+# Taken From StackOverflow
+# https://stackoverflow.com/questions/7287014/is-there-any-drand48-equivalent-in-python-or-a-wrapper-to-it
 
+class Rand48(object):
+    def __init__(self, seed):
+        self.n = seed
+    def seed(self, seed):
+        self.n = seed
+    def srand(self, seed):
+        self.n = (seed << 16) + 0x330e
+    def next(self):
+        self.n = (25214903917 * self.n + 11) & (2**48 - 1)
+        return self.n
+    def drand(self):
+        return self.next() / 2**48
+    def lrand(self):
+        return self.next() >> 17
+    def mrand(self):
+        n = self.next() >> 16
+        if n & (1 << 31):
+            n -= 1 << 32
+        return n 
+
+############################################################################
 
 
 
@@ -37,6 +61,11 @@ if __name__ == "__main__":
 	if sys.argv[8] == "BEGINNING":
 		rr_add = sys.argv[8]
 
+
+	rand = Rand48(0)
+	rand.srand(seed)
+	print(rand.drand())
+	print(rand.drand())
 
 
 
